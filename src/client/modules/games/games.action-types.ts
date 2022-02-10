1491b4ce9c155b5616d754c2a7a41239af815c19
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-02-10 17:11:21 +0700

    create game action types enum

---
+++ src/client/modules/games/games.action-types.ts
@@ -0,0 +1,8 @@
+export enum GamesActTypes {
+    download = 'download',
+    addGame = 'addGame',
+    deleteGame = 'deleteGame',
+    changeNumber = 'changeNumber',
+    sortByAlphabetically = 'sortByAlphabetically',
+    sortByNumber = 'sortByNumber'
+}
